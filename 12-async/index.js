cbcfaa1a17c8fc69885922e9b9cebdb2aba6d624
auzanassdq <auzanassidqi@gmail.com>
2022-06-15 11:58:35 +0700

    show to html

--- 12-async/index.js
+++ 12-async/index.js
@@ -32,32 +32,47 @@ const p3 = () => {
 
 p3();
 
+let photoProfile = document.getElementById("photoProfile");
+let username = document.getElementById("username");
+let followers = document.getElementById("followers");
+
 // promise fetch
-const data = {};
-const getData = () => {
-  // get data API
-  const url = "https://api.github.com/users/bilasta89";
+// get data API
+// const getDataGithubPromise = () => {
+const url = "https://api.github.com/users/bilasta89";
 
-  // melakukan fetch data
-  fetch(url)
-    // membuka paket
-    .then((response) => response.json())
-    // mengecheck hasil paketnya sudah sesuai atau belum
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error));
-};
+// melakukan fetch data
+fetch(url)
+  .then((response) => response.json()) // membuka paket
+  .then((result) => {
+    console.log(result);
+    photoProfile.src = result.avatar_url;
+    username.innerText = result.login;
+    followers.innerText = result.followers;
+  })
+  .catch((error) => console.log(error));
+// };
 
-getData();
+// getDataGithubPromise()
+// console.log(data);
 
 // async fetch
-const getDataGithub = async () => {
+const getDataGithubAsync = async () => {
   const url = "https://api.github.com/users/bilasta89";
 
   // melakukan fetch data
   let response = await fetch(url);
   // membuka paket
-  response = await response.json();
-  console.log(response);
+  let result = await response.json();
+  console.log(result);
+
+  photoProfile.src = result.avatar_url;
+  username.innerText = result.login;
+  followers.innerText = result.followers;
+
+  // function async jika di dalamnya dikasih return
+  // maka function ini akan bernilai sebuah promise
+  // return result
 };
 
-getDataGithub();
+getDataGithubAsync();
