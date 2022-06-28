3cd08783633b40d847ba94824afa6b0778526a1e
thoriqnf <thoriqnfaizal@gmail.com>
2022-06-28 11:24:42 +0700

    adding card course

--- 19-react/src/components/Course/Course.js
+++ 19-react/src/components/Course/Course.js
@@ -4,9 +4,29 @@ const Course = () => {
   return (
     <div>
       <h1>Course</h1>
-      <div style={{ display: "flex" }}>
-        <CourseCard />
-        <CourseCard />
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <CourseCard
+          title="How to be youtuber"
+          detail="Tips and trick menjadi Youtuber"
+          embedId="rokGy0huYEA"
+        />
+
+        <CourseCard
+          title="programmer"
+          detail="Tips and trick menjadi Youtuber"
+          embedId="qrlj6tijDlY"
+        />
+
+        <CourseCard
+          title="How to be artist"
+          detail="Tips and trick menjadi Youtuber"
+        />
+
+        <CourseCard
+          title="How to be CEO"
+          detail="Tips and trick menjadi Youtuber"
+        />
+        {/* <CourseCard /> */}
       </div>
     </div>
   );
