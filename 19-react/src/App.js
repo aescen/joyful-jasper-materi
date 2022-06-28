cc653d48af2e2e5fd09e415dab4375af6351f757
thoriqnf <thoriqnfaizal@gmail.com>
2022-06-28 09:49:22 +0700

    adding exercise react

--- 19-react/src/App.js
+++ 19-react/src/App.js
@@ -1,45 +1,55 @@
 import "./App.css";
 import HelloWorld from "./components/HelloWorld.jsx";
-import catImage from "./cat.png";
+import Button from "./components/Button";
+
+// import catImage from "./cat.png";
 
 const App = () => {
-  // sebelum return tempat mendeklarasikan JS
+  // tempat mendeklarasikan JS sebelum return
   let tes = "tess";
   let num = 1 + 1000;
 
+  // external handle event
   const handleClick = () => {
     console.log("tes");
   };
 
   const isLogin = false;
 
-  const data = [
-    {
-      murid: "mirza",
-    },
-    {
-      murid: "miana",
-    },
-    {
-      murid: "hadi",
-    },
-    {
-      murid: "arrie",
-    },
-  ];
+  // const data = [
+  //   {
+  //     murid: "dila",
+  //   },
+  //   {
+  //     murid: "auzan",
+  //   },
+  // ];
+
+  const data = ["dila", "auzan"];
+
   return (
     // JSX
     <div className="App">
       <header className="App-header">
+        {data.map((item, key) => (
+          <h1 key={key}>{item}</h1>
+        ))}
+        <Button />
         <HelloWorld />
         <p>Hello World Joyfull Jasper!</p>
         <p>{tes.toUpperCase()}</p>
         <p>{num}</p>
+
+        {/* image from url */}
         {/* <img
           src="https://th.bing.com/th/id/OIP.krXgDcts2hh2AT61oGCDrgHaE8?w=269&h=180&c=7&r=0&o=5&dpr=2&pid=1.7"
           alt="cat"
         /> */}
+
+        {/* image from local */}
         {/* <img src={catImage} alt="cat" /> */}
+
+        {/* external handle event*/}
         <button onClick={handleClick}>Tes button</button>
         <button onClick={() => console.log("tes inline")}>
           Tes button inline
---
+++ 19-react/src/a.js
@@ -0,0 +1,4 @@
+// Buat sebuah komponen dalam aplikasi react kalian bernama "button.js"
+// Dalam komponen button berikan handleClick event, bebas mau internal atau external
+// Buat sebuah data pada app.js berisi nama teman kalian kelompok web basic
+// tampilkan nama mereka dalam tag h1
