3b0f7553708810af2301a4a91ccc2fa5eeda2839
thoriqnf <thoriqnfaizal@gmail.com>
2022-07-06 13:03:06 +0700

    github context

--- 24-context/src/App.js
+++ 24-context/src/App.js
@@ -3,10 +3,15 @@ import "./App.css";
 import User from "./components/User";
 
 import UserContextProvider from "./context/UserContext";
+import GithubContextProvider from "./context/GithubContext";
+import APIGithub from "./components/APIGithub";
 
 function App() {
   return (
     <div className="App">
+      <GithubContextProvider>
+        <APIGithub />
+      </GithubContextProvider>
       <UserContextProvider>
         <h1>React Context</h1>
         <User />
